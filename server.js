bdc17a57baa7b904a62d7fb0b4a693f2704865a2
rajneesh <rkv7681999@gmail.com>
2024-08-15 20:53:45 +0530

    add-new-one

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const formRoutes = require('./routes/formRoutes');
+const path = require("path")
 
 dotenv.config();
 
@@ -32,25 +33,10 @@ const app = express();
 connectDB();
 
 
+app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
  const allowedOrigins = ['https://xinom-front.vercel.app'];
 
 // const allowedOrigins = ["http://localhost:3000"]
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
-
-// app.use(express.json());
-
 
 const corsOptions = {
     origin: function (origin, callback) {
