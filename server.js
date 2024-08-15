2e80affb55676b8059077117fac250c1e2771dbd
rajneesh <rkv7681999@gmail.com>
2024-08-15 20:31:50 +0530

    add-new-one

--- server.js
+++ server.js
@@ -24,48 +24,67 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const formRoutes = require('./routes/formRoutes');
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Initialize express app
+
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// CORS configuration to accept requests from the frontend URL
-const allowedOrigins = ['https://xinom-front.vercel.app'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+ const allowedOrigins = ['https://xinom-front.vercel.app'];
 
-// Middleware to parse incoming JSON requests
+// const allowedOrigins = ["http://localhost:3000"]
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+
+// app.use(express.json());
+
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
+
 app.use(express.json());
 
-// Route for handling form-related requests
 app.use('/api/form', formRoutes);
 
-// Port from environment variable or default to 4000
+
 const PORT = process.env.PORT || 4000;
 
-// Start the server
+
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-// Basic error handling for unhandled promise rejections
+
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Unhandled Rejection: ${err.message}`);
-  // Close the server and exit process
+
   server.close(() => process.exit(1));
 });
 
